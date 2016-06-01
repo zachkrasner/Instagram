11f9365f93fece7135f2ad2a6709e30be655cc2a
zachkrasner <zachkrasner@gmail.com>
2016-06-01 13:31:21 -0700

    Added Delete feature. Also auto-sets post id before methods in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -13,18 +15,23 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to(post_path(@post))
   end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
 
   private
 
@@ -32,4 +39,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:image, :caption)
   end
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
